1ec3634ade18bb40ae8bb5d8305f8730648ccb26
unknown <nisha.pawar2231@gmail.com>
2024-05-12 17:15:22 +0530

    first commit

---
+++ fullnamedisplay/src/App.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [fname, setFname] = useState("");
+  const [lname, setLname] = useState("");
+  const [isSubmit, setIsSubmit] = useState(false);
+
+  const onsubmithandler = (e) => {
+    e.preventDefault();
+    setIsSubmit(true);
+  };
+  return (
+    <div className="App">
+      <h1>Full Name Display</h1>
+      <form onSubmit={onsubmithandler}>
+        <label for="firstname">First Name:</label>
+        <input
+          type="text"
+          id="firstname"
+          required
+          onChange={(e) => setFname(e.target.value)}
+        />
+        <br />
+        <label for="lastname">Last Name:</label>
+        <input
+          type="text"
+          id="lastname"
+          required
+          onChange={(e) => {
+            setLname(e.target.value);
+          }}
+        />
+        <br />
+        <button>Submit</button>
+      </form>
+      {isSubmit ? (
+        <p>
+          Full Name: {fname} {lname}
+        </p>
+      ) : (
+        <p></p>
+      )}
+    </div>
+  );
+}
+
+export default App;
